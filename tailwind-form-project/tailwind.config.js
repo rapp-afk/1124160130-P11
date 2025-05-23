cca9be5cbee155b910238f665378703ba648fd89
rapp-afk <@rafafathurrohman42@gmail.com>
2025-05-23 11:30:19 +0700

    membuat folder dan menginstall tailwind

---
+++ tailwind-form-project/tailwind.config.js
@@ -0,0 +1,9 @@
+/**@type{import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',// Aktifkan dark mode dengan class
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
