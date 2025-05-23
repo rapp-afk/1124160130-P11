ef14066b3371fbe612ea6de422a47e91a9ac7535
rapp-afk <@rafafathurrohman42@gmail.com>
2025-05-23 12:44:23 +0700

    menambahkan code login dan entry data

--- tailwind-form-project/tailwind.config.js
+++ tailwind-form-project/tailwind.config.js
@@ -1,7 +1,7 @@
-/**@type{import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  darkMode: 'class',// Aktifkan dark mode dengan class
+  content: [".//.html", ".//.js"],
+    darkMode: 'class',// Aktifkan dark mode dengan class
   theme: {
     extend: {},
   },
